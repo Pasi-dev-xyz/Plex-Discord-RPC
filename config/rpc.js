58ebe5b6bb41267b4b32150c6f0f70350a18b99c
Pasi-dev.xyz <pasi@pasi-dev.xyz>
2021-07-09 20:04:25 +0200

    Recode Plex DiscordRPC

---
+++ config/rpc.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const config = {};
+
+config.rpc = {
+  id: '',
+  updateInterval: 10000,
+  connectionTimeout: 25000,
+};
+
+module.exports = config;
---
+++ src/plex_discord_rpc.js
@@ -0,0 +1,5 @@
+'use strict';
+
+require('./rpc/client');
+
+console.log('started.');
